df15d446af21f12e827641c5eb841d0a11b27ce8
iamstan13y <iamstan13y@gmail.com>
2023-04-25 23:49:22 +0200

    Essential Services Config.

---
+++ BevAPI/Services/JwtService.cs
@@ -0,0 +1,38 @@
+﻿using BevAPI.Models.Data;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BevAPI.Services
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration) => _configuration = configuration;
+
+        public Task<string> GenerateTokenAsync(Account account)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["JwtService:Secret"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("UserId", account.Id.ToString()),
+                    new Claim(ClaimTypes.Email, account.Email!),
+                    new Claim(ClaimTypes.GivenName, account.FullName!)
+                }),
+
+                Expires = DateTime.Now.AddHours(8),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            account.Token = tokenHandler.WriteToken(token);
+
+            return Task.FromResult(account.Token);
+        }
+    }
+}
