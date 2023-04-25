69f4f001c9c6c3aea4926ee47e23b2bedfd2d1ee
iamstan13y <iamstan13y@gmail.com>
2023-04-25 20:51:05 +0200

    HTTP Request pipeline Config.

--- BevAPI/Program.cs
+++ BevAPI/Program.cs
@@ -1,6 +1,12 @@
+using BevAPI.Models.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
