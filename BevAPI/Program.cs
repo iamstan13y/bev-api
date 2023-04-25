0eae4c565a18bebb2f7fb0fce5f7864edc727eee
iamstan13y <iamstan13y@gmail.com>
2023-04-25 21:38:00 +0200

    Unit of Work Config & DI.

--- BevAPI/Program.cs
+++ BevAPI/Program.cs
@@ -1,4 +1,5 @@
 using BevAPI.Models.Data;
+using BevAPI.Models.Repository.IRepository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -11,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, IUnitOfWork>();
 
 builder.Services.AddControllers();
 
