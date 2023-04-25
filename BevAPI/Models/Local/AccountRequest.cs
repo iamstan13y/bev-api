cf930e496f9dffe09eb9e46b544ffd076f478225
iamstan13y <iamstan13y@gmail.com>
2023-04-26 00:03:11 +0200

    Add Account Implementation.

---
+++ BevAPI/Models/Local/AccountRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace BevAPI.Models.Local
+{
+    public class AccountRequest
+    {
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+    }
+}
