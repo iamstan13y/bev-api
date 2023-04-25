cf989adc56e1a11ee777e4e40fff6bfa68892301
iamstan13y <iamstan13y@gmail.com>
2023-04-25 21:37:51 +0200

    Unit of Work Config & DI.

---
+++ BevAPI/Models/Repository/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using BevAPI.Models.Data;
+
+namespace BevAPI.Models.Repository
+{
+    public class UnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void SaveChanges() => _context.SaveChanges();
+    }
+}
