3809787d714cd2c44f77c90550ae3e93e0fb6aca
iamstan13y <iamstan13y@gmail.com>
2023-04-26 00:03:26 +0200

    Add Account Implementation.

--- BevAPI/Models/Repository/IRepository/IAccountRepository.cs
+++ BevAPI/Models/Repository/IRepository/IAccountRepository.cs
@@ -1,7 +1,17 @@
-﻿namespace BevAPI.Models.Repository.IRepository
+﻿using BevAPI.Models.Data;
+using BevAPI.Models.Local;
+using BevAPI.Utility;
+
+namespace BevAPI.Models.Repository.IRepository
 {
     public interface IAccountRepository
     {
-
+        Task<Result<Account>> AddAsync(Account account);
+        Task<Result<Account>> GetByIdAsync(int id);
+        Task<Result<IEnumerable<Account>>> GetAllAsync();
+        Task<Result<Account>> UpdateAsync(Account account);
+        Task<Result<bool>> DeleteAsync(Account account);
+        Task<Result<Account>> LoginAsync(LoginRequest request);
+        //Task<Result<Account>> ChangePasswordAsync(ChangePasswordRequest changePassword);
     }
 }
