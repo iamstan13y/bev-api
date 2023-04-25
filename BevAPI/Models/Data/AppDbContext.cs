f2676661d2710e690689fa777bb37298eab605aa
iamstan13y <iamstan13y@gmail.com>
2023-04-25 23:03:30 +0200

    Acocunt Model & DB Migrations.

--- BevAPI/Models/Data/AppDbContext.cs
+++ BevAPI/Models/Data/AppDbContext.cs
@@ -6,7 +6,8 @@ namespace BevAPI.Models.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
-
         }
+
+        public DbSet<Account>? Accounts { get; set; }
     }
 }
