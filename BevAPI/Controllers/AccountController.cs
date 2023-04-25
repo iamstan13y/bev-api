cc1f44897f4c386087695b32a99365a3804ff072
iamstan13y <iamstan13y@gmail.com>
2023-04-26 00:03:03 +0200

    Add Account Implementation.

---
+++ BevAPI/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using BevAPI.Models.Data;
+using BevAPI.Models.Local;
+using BevAPI.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BevAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountController(IAccountRepository accountRepository) => _accountRepository = accountRepository;
+        
+        [HttpPost]
+        public async Task<IActionResult> Post(AccountRequest request)
+        {
+            var result = await _accountRepository.AddAsync(new Account
+            {
+                FullName = request.FullName,
+                Email = request.Email,
+                Password = request.Password,
+                PhoneNumber = request.PhoneNumber
+            });
+
+            if(!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
+    }
+}
