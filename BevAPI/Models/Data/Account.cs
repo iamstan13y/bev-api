ba5407db00691cc6aeaa82653b3a6ef78f599768
iamstan13y <iamstan13y@gmail.com>
2023-04-25 23:03:24 +0200

    Acocunt Model & DB Migrations.

---
+++ BevAPI/Models/Data/Account.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BevAPI.Models.Data
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Password { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        [NotMapped]
+        public string? Token { get; set; }
+    }
+}
