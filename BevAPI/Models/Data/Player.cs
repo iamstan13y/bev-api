bd4b5e2bfc6c676f01c32243aad3b91c3b58d48c
iamstan13y <iamstan13y@gmail.com>
2023-04-26 19:07:41 +0200

    Player Model & DB Migrations.

---
+++ BevAPI/Models/Data/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace BevAPI.Models.Data
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+    }
+}
