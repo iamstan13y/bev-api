e1790fe6d8ccbe4568799949b50f0cf5e0d85293
iamstan13y <iamstan13y@gmail.com>
2023-04-27 19:21:01 +0200

    Player Kit & DB Migrations.

---
+++ BevAPI/Migrations/20230427172023_PlayerKitToDb.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BevAPI.Migrations
+{
+    public partial class PlayerKitToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "KitNumber",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Position",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KitNumber",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Position",
+                table: "Players");
+        }
+    }
+}
