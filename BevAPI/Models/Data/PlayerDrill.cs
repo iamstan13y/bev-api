448759db922fe0c4a779c7e4b4a99c5e48779d01
iamstan13y <iamstan13y@gmail.com>
2023-04-27 23:19:18 +0200

    Player Drill & DB Migrations.

---
+++ BevAPI/Models/Data/PlayerDrill.cs
@@ -0,0 +1,10 @@
+﻿namespace BevAPI.Models.Data
+{
+    public class PlayerDrill
+    {
+        public int Id { get; set; }
+        public int PlayerId { get; set; }
+        public int DrillMark { get; set; }
+        public Player? Player { get; set; }
+    }
+}
