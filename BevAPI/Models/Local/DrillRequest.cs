8f5122d5d04d2e4eb84286ed6db363e00a2578c5
iamstan13y <iamstan13y@gmail.com>
2023-04-27 22:59:51 +0200

    Update Drill Request

---
+++ BevAPI/Models/Local/DrillRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace BevAPI.Models.Local
+{
+    public class DrillRequest
+    {
+        public string? Type { get; set; }
+        public int MaxMark { get; set; }
+        public int MinMark { get; set; }
+    }
+
+    public class UpdateDrillRequest : DrillRequest
+    {
+        public int Id { get; set; }
+    }
+}
