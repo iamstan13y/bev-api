cd13f785ecb49a061239ef20339d094bebd89270
iamstan13y <iamstan13y@gmail.com>
2023-04-27 22:47:02 +0200

    Drill Repository Pattern & UoW DI.

--- BevAPI/Models/Repository/UnitOfWork.cs
+++ BevAPI/Models/Repository/UnitOfWork.cs
@@ -8,10 +8,12 @@ namespace BevAPI.Models.Repository
         private readonly AppDbContext _context;
 
         public IPlayerRepository Player { get; private set; }
+        public IDrillRepository Drill { get; private set; }
 
         public UnitOfWork(AppDbContext context)
         {
             Player = new PlayerRepository(context);
+            Drill = new DrillRepository(context);
 
             _context = context;
         }
