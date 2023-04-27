6c1c50b5ab60cb0941bb398ae8f2c94f2fe923fd
iamstan13y <iamstan13y@gmail.com>
2023-04-27 23:31:20 +0200

    Player Repository & DB Migration.

---
+++ BevAPI/Models/Repository/PlayerDrillRepository.cs
@@ -0,0 +1,12 @@
+﻿using BevAPI.Models.Data;
+using BevAPI.Models.Repository.IRepository;
+
+namespace BevAPI.Models.Repository
+{
+    public class PlayerDrillRepository : Repository<PlayerDrill>, IPlayerDrillRepository
+    {
+        public PlayerDrillRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
