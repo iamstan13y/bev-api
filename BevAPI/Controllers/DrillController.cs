37b12111ab05b96b9e416e5b3be2ef092721b134
iamstan13y <iamstan13y@gmail.com>
2023-04-27 22:49:27 +0200

    Drill Controller Constructor.

--- BevAPI/Controllers/DrillController.cs
+++ BevAPI/Controllers/DrillController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BevAPI.Models.Data;
+using BevAPI.Models.Local;
+using BevAPI.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BevAPI.Controllers
 {
@@ -6,5 +9,61 @@ namespace BevAPI.Controllers
     [ApiController]
     public class DrillController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DrillController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(await _unitOfWork.Player.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _unitOfWork.Player.FindAsync(id);
+            if (!result.Success) return NotFound(result);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(PlayerRequest request)
+        {
+            var result = await _unitOfWork.Player.AddAsync(new Player
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Position = request.Position,
+                Height = request.Height,
+                Weight = request.Weight,
+                KitNumber = request.KitNumber
+            });
+
+            _unitOfWork.SaveChanges();
+
+            if (!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(UpdatePlayerRequest request)
+        {
+            var result = await _unitOfWork.Player.UpdateAsync(new Player
+            {
+                Id = request.Id,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Position = request.Position,
+                Height = request.Height,
+                Weight = request.Weight,
+                KitNumber = request.KitNumber
+            });
+
+            _unitOfWork.SaveChanges();
+
+            if (!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
     }
 }
