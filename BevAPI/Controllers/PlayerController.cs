6c1013cd6aeccc7d49ebf5026ba04fe224ba7c85
iamstan13y <iamstan13y@gmail.com>
2023-04-27 19:59:59 +0200

    Added Player Comtroller.

---
+++ BevAPI/Controllers/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using BevAPI.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BevAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PlayerController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PlayerController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
