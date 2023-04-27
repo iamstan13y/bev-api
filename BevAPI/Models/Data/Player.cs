bd108c12cdc14b574dd210158d5f034c593d5b17
iamstan13y <iamstan13y@gmail.com>
2023-04-27 19:21:19 +0200

    Player Kit & DB Migrations.

--- BevAPI/Models/Data/Player.cs
+++ BevAPI/Models/Data/Player.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public string? Position { get; set; }
+        public int KitNumber { get; set; }
         public double Weight { get; set; }
         public double Height { get; set; }
     }
