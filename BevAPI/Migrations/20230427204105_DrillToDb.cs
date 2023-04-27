f614a6c7d13745e9d5689dadcb60c0f06b13a4d0
iamstan13y <iamstan13y@gmail.com>
2023-04-27 22:41:34 +0200

    Drill Model & DB Migrations.

---
+++ BevAPI/Migrations/20230427204105_DrillToDb.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BevAPI.Migrations
+{
+    public partial class DrillToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaxMark = table.Column<int>(type: "int", nullable: false),
+                    MinMark = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drills", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Drills");
+        }
+    }
+}
