6fc7989759ff894a25ed9386514fb992945574f5
iamstan13y <iamstan13y@gmail.com>
2023-04-27 19:43:34 +0200

    Player Repository & UoW DI.

---
+++ BevAPI/Models/Repository/PlayerRepository.cs
@@ -0,0 +1,12 @@
+﻿using BevAPI.Models.Data;
+using BevAPI.Models.Repository.IRepository;
+
+namespace BevAPI.Models.Repository
+{
+    public class PlayerRepository : Repository<Player>, IPlayerRepository
+    {
+        public PlayerRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
