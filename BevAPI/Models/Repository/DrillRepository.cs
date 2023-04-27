2e1d28d757ba9655b4eba508a60e8306c3207d51
iamstan13y <iamstan13y@gmail.com>
2023-04-27 22:47:00 +0200

    Drill Repository Pattern & UoW DI.

---
+++ BevAPI/Models/Repository/DrillRepository.cs
@@ -0,0 +1,12 @@
+﻿using BevAPI.Models.Data;
+using BevAPI.Models.Repository.IRepository;
+
+namespace BevAPI.Models.Repository
+{
+    public class DrillRepository : Repository<Drill>, IDrillRepository
+    {
+        public DrillRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
