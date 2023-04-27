bde93359240122decf72444111040d8e30e874df
iamstan13y <iamstan13y@gmail.com>
2023-04-27 22:41:53 +0200

    Drill Model & DB Migrations.

---
+++ BevAPI/Models/Data/Drill.cs
@@ -0,0 +1,10 @@
+﻿namespace BevAPI.Models.Data
+{
+    public class Drill
+    {
+        public int Id { get; set; }
+        public string? Type { get; set; }
+        public int MaxMark { get; set; }
+        public int MinMark { get; set; }
+    }
+}
