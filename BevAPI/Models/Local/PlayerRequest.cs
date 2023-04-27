f65fadd1d2d1e55b479649aeb31dd892acd6b11f
iamstan13y <iamstan13y@gmail.com>
2023-04-27 20:26:16 +0200

    Add Player Implementation.

---
+++ BevAPI/Models/Local/PlayerRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace BevAPI.Models.Local
+{
+    public class PlayerRequest
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Position { get; set; }
+        public int KitNumber { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+    }
+}
