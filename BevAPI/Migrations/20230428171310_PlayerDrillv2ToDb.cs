865248b6e2249ce68d9ece14555171bc9c159a24
iamstan13y <iamstan13y@gmail.com>
2023-04-28 19:14:36 +0200

    Player Drill fixes & DB Migrations.

---
+++ BevAPI/Migrations/20230428171310_PlayerDrillv2ToDb.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BevAPI.Migrations
+{
+    public partial class PlayerDrillv2ToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DrillId",
+                table: "PlayerDrills",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerDrills_DrillId",
+                table: "PlayerDrills",
+                column: "DrillId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PlayerDrills_Drills_DrillId",
+                table: "PlayerDrills",
+                column: "DrillId",
+                principalTable: "Drills",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PlayerDrills_Drills_DrillId",
+                table: "PlayerDrills");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PlayerDrills_DrillId",
+                table: "PlayerDrills");
+
+            migrationBuilder.DropColumn(
+                name: "DrillId",
+                table: "PlayerDrills");
+        }
+    }
+}
