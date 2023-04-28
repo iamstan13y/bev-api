944024e4fc92e2486dadf3b7bc32da5f4dee9262
iamstan13y <iamstan13y@gmail.com>
2023-04-28 10:12:41 +0200

    Player Drill Controller Constructor.

--- BevAPI/Controllers/PlayerDrillController.cs
+++ BevAPI/Controllers/PlayerDrillController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BevAPI.Models.Data;
+using BevAPI.Models.Local;
+using BevAPI.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BevAPI.Controllers
 {
@@ -6,5 +9,53 @@ namespace BevAPI.Controllers
     [ApiController]
     public class PlayerDrillController : ControllerBase
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PlayerDrillController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+        [HttpGet]
+        public async Task<IActionResult> Get() => Ok(await _unitOfWork.Drill.GetAllAsync());
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _unitOfWork.Drill.FindAsync(id);
+            if (!result.Success) return NotFound(result);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(DrillRequest request)
+        {
+            var result = await _unitOfWork.Drill.AddAsync(new Drill
+            {
+                MaxMark = request.MaxMark,
+                MinMark = request.MinMark
+            });
+
+            _unitOfWork.SaveChanges();
+
+            if (!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(UpdateDrillRequest request)
+        {
+            var result = await _unitOfWork.Drill.UpdateAsync(new Drill
+            {
+                Id = request.Id,
+                MaxMark = request.MaxMark,
+                MinMark = request.MinMark
+            });
+
+            _unitOfWork.SaveChanges();
+
+            if (!result.Success) return BadRequest(result);
+
+            return Ok(result);
+        }
     }
 }
